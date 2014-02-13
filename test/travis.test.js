3aa6d11c8996950c0f76497430c727305f945f87
Alexis Boissonnat <alexis.boissonnat@gmail.com>
2014-02-13 15:39:46 +0100

    Add the testing framework : mocha

---
+++ test/travis.test.js
@@ -0,0 +1,6 @@
+var assert = require("assert")
+describe('Use dummy test to validate the entire CI round trip', function(){
+    it('should count 6 letters in <foobar>', function(){
+        assert.equal(6, "foobar".length);
+    })
+})
