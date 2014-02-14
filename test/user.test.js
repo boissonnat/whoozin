483b6dae54d3b1fc5f471b0b03c89d69dbcc1e9e
Alexis Boissonnat <alexis.boissonnat@gmail.com>
2014-02-14 09:47:36 +0100

    Add should.js as test framework over mocha

--- test/user.test.js
+++ test/user.test.js
@@ -1,4 +1,5 @@
 var Parse = require('parse').Parse;
+var should = require('should');
 
 // Due to travis-ci, we have to expose our Parse.com keys...
 // To handle this issue, we are using specific Parse App where data can be destroyed at any time.
@@ -7,74 +8,78 @@ var PARSE_SDK_KEY = "HpGn9ocefXikSOX7LkBMyuDTrf3qeGgYjvy8Ddrd";
 
 Parse.initialize(PARSE_APP_KEY, PARSE_SDK_KEY);
 
-describe('User', function(){
+describe('User', function () {
 
-    after(function(done){
+    after(function (done) {
         console.log("Delete foobar after tests...")
         var userQuery = new Parse.Query(Parse.User);
         userQuery.equalTo("username", "foobar");
         userQuery.first({
-            success: function(user){
+            success: function (user) {
                 user.destroy({
-                    success: function(object){
+                    success: function (object) {
                         console.log("...User foobar successfully deleted");
                         done();
                     },
-                    error: function(object, error){
+                    error: function (object, error) {
                         console.log("...Unable to delete foobar ! Fix it now to be able to run the tests again");
                         throw error;
                     }
                 })
             },
-            error: function(error){
+            error: function (error) {
                 console.log("...Weird : No user corresponding to foobar");
                 throw error;
             }
         })
-    })
+    });
 
-    describe('#save()', function(){
-        it('should save without error', function(done){
+    describe('#save()', function () {
+        it('should save without error', function (done) {
             var user = new Parse.User();
             user.set('username', 'foobar');
             user.set('email', 'foo@bar.com');
             user.set('password', 'foobar');
             user.signUp(null, {
-                success: function(user) {
+                success: function (user) {
+                    user.get("username").should.equal('foobar');
+                    user.get("email").should.equal('foo@bar.com');
                     done();
                 },
-                error: function(user, error) {
-                    console.log("...Unable to create foobar user")
+                error: function (user, error) {
+                    console.log("...Unable to create foobar user");
                     throw error;
                 }
             });
-        })
-        it('should not be able to save foobar again', function(done){
+        });
+        it('should not be able to save foobar again', function (done) {
             var user = new Parse.User();
             user.set('username', 'foobar');
             user.set('email', 'foo@bar.com');
             user.set('password', 'foobar');
             user.signUp(null, {
-                success: function(user) {
+                success: function (user) {
                     throw 'Foobar saved again ! This should not append';
                 },
-                error: function(user, error) {
+                error: function (user, error) {
                     done();
                 }
             });
         })
-    })
+    });
 
-    describe('#logIn()', function(){
-        it('should be log in foobar', function(done){
-            Parse.User.logIn("foobar", "foobar", {
-                success: function(user) {
+    describe('#logIn()', function () {
+        it('should be log in foobar', function (done) {
+            var user = Parse.User.logIn("foobar", "foobar", {
+                success: function (user) {
+                    user.get("username").should.equal('foobar');
+                    user.get("email").should.equal('foo@bar.com');
                     done();
                 },
-                error: function(user, error) {
+                error: function (user, error) {
                     throw error;
                 }
             });
         })
     })
-})
+});
