c3f5ada4025822884e807fb3db9d4e2a9677172e
Alexis Boissonnat <alexis.boissonnat@gmail.com>
2014-02-18 15:10:29 +0100

    Add Error handling

--- app.js
+++ app.js
@@ -39,10 +39,10 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-// Secure URL through middleware
+// MIDDLEWARE
 ////////////////////////////////////////
 
-// Ensure user is logged in
+// Secure URL through middleware
 function requireUser(req, res, next) {
   if (Parse.User.current()) {
     // Ok user is logged in, allow the next route to run
@@ -51,12 +51,54 @@ function requireUser(req, res, next) {
   } else {
     // No user found -> 403
     console.log("requireUser() called, no user present -> 403");
-    res.send(403);
+    res.render("errors/403")
   }
 }
 
-// Define routes
+// Error handlers
+app.use(function(req, res, next){
+  res.status(404);
+  // respond with html page
+  if (req.accepts('html')) {
+    res.render('errors/404', { url: req.url });
+    return;
+  }
+  // respond with json
+  if (req.accepts('json')) {
+    res.send({ error: 'Not found' });
+    return;
+  }
+  // default to plain-text. send()
+  res.type('txt').send('Not found');
+});
+
+app.use(function(err, req, res, next){
+  res.status(err.status || 500);
+  res.render('errors/500', { error: err });
+});
+
+
+
+// ROUTES
 ///////////////////////////////////////
+
+// Errors
+app.get('/404', function(req, res, next){
+  // trigger a 404 since no other middleware will match /404 after this one
+  next();
+});
+
+app.get('/403', function(req, res, next){
+  // trigger a 403 error
+  var err = new Error('not allowed!');
+  err.status = 403;
+  next(err);
+});
+
+app.get('/500', function(req, res, next){
+  // trigger a generic (500) error
+  next(new Error('keyboard cat!'));
+});
 
 // Root
 app.get('/', routes.index(Parse));
@@ -74,6 +116,7 @@ app.post('/activities/save', requireUser, activities.save(Parse));
 app.get('/activities/:id', activities.show(Parse));
 
 
+// Launch the server
 http.createServer(app).listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
 });
